c52bae4995677271295be1f0aee9a6d74607bb69
santiiiespinoza1 <55319567+santiiiespinoza1@users.noreply.github.com>
2021-06-06 10:30:22 -0300

    Update code.js

--- code.js
+++ code.js
@@ -1,15 +1,25 @@
 jQuery('document').ready(function($){
 
     var menuBtn= $('.nav-icon'),
-        menu=$('.navegacion ul');
+        menu=$('.navegacion ul'),
+        shopNow = $('.shop-now');
 
+    if (menu.hasClass('show')){
+    	menu.removeClass('show');
+    }
     menuBtn.click(function(){
         if(menu.hasClass('show')){
             menu.removeClass('show');
+            shopNow.css('display','block');
         }else{
             menu.addClass('show');
+            shopNow.css('display','none');
         }
 
     });
 
 });
+
+
+
+
